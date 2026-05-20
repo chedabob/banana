8b189ab9864c6e7e22aa632fbaf67c9fce72672a
chedabob <matt.malone1@gmail.com>
2026-05-20 08:02:35 +0200

    chore: bump service worker to v3 to force cache flush

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'banana-detector-v2';
+const CACHE = 'banana-detector-v3';
 const PRECACHE = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
